46435153ec3da13bcb31580647229da08717776c
moshfox <cacatomica69@gmail.com>
2025-06-14 00:43:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 // --- Datos de las Canciones ---
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch(error => {
-        console.error('Fallo el registro del Service Worker:', error);
-      });
-  });
+
 const songs = [
     {
         id: 'hawaiian',
